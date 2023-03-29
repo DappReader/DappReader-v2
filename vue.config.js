1910fb18a4981f0cc0546910ea0aa6db778ce403
Tiyo <tiyosheng@gmail.com>
2023-03-30 00:14:31 +0800

    fix bug

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   publicPath: './',
+  productionSourceMap: process.env.NODE_ENV === 'production' ? false : true,
   configureWebpack: (config)=>{
     if(process.env.NODE_ENV === 'production'){
       // config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
